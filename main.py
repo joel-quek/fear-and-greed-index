b89df7dc350e1546703f90eb4c1800913ac48004
Joel Quek <103588217+joel-quek@users.noreply.github.com>
2022-12-03 02:37:16 +0800

    gsgff

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import send_file
 from flask import render_template
 import io
 import base64
+
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import yfinance as yf
+import requests
+import pandas as pd
 
 # SOURCE OF THIS: Visualize with Seaborn and Matplotlib on Flask https://www.youtube.com/watch?v=shSqnROZ1o8 
 
@@ -33,6 +38,27 @@ def visualise():
     fig.savefig(img)
     img.seek(0)
     return send_file(img,mimetype='img/png')
+
+# This part is new
+@app.route('/technical')
+def technical():
+    goog_stock_data = yf.download('GOOG','2004-08-19') # dataframe
+    sns.lineplot(goog_stock_data.index,goog_stock_data['Close'])
+    canvas=FigureCanvas(fig)
+    img=io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img,mimetype='img/png')
+    '''
+    plt.figure(figsize=(15, 15))
+    plt.ylabel('GOOG Closing Price')
+    goog_stock_data.Close.plot()
+    # https://stackoverflow.com/questions/26132693/matplotlib-saving-state-between-different-uses-of-io-bytesio
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return send_file(buf,mimetype='img/png') # buf.read() 
+    '''
 
 if __name__=='__main__':
     app.debug = True
